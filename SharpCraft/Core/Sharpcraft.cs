9324a5288e6315a4d89658afa8397d3dbeb725e3
Myo-kun <myo.kun345@gmail.com>
2013-09-21 00:28:35 -0400

    Some attempts at starting a GameWindow

--- SharpCraft/Core/Sharpcraft.cs
+++ SharpCraft/Core/Sharpcraft.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 using Microsoft.Practices.EnterpriseLibrary.Logging;
 
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
 namespace MyoKun.SharpCraft.Core
 {
     public class Sharpcraft
@@ -13,6 +17,8 @@ namespace MyoKun.SharpCraft.Core
         public string Username;
         public LogWriter Writer = new LogWriterFactory().Create();
         private Sharpcraft Instance;
+
+        public SharpcraftWindow Window;
 
         public Boolean Running;
 
@@ -24,17 +30,44 @@ namespace MyoKun.SharpCraft.Core
             Console.WriteLine(Username);
         }
 
+        private void StartGame()
+        {
+            Window = new SharpcraftWindow(800, 600, "Sharpcraft", Instance);
+        }
+
+        private void RunGameLoop()
+        {
+
+        }
+
+        protected void handleKey(object sender, KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.StopGame(0x00);
+            }
+        }
+
+        public void StopGame(int ExitCode)
+        {
+            Window.Exit();
+            Writer.Write("Sharpcraft exited with code " + ExitCode, ExitCode != 0x00 ? "SEVERE" : "FINE");
+            Environment.Exit(ExitCode);
+            System.GC.Collect();
+        }
+
         public void Run()
         {
             this.Running = true;
 
             try
             {
-                //this.StartGame();
+                this.StartGame();
             }
             catch (Exception e)
             {
                 Writer.Write(e.ToString(), "SEVERE");
+                this.StopGame(0x01);
             }
 
             try
@@ -45,11 +78,12 @@ namespace MyoKun.SharpCraft.Core
                     {
                         try
                         {
-                            //this.RunGameLoop;
+                            //this.RunGameLoop();
                         }
                         catch (OutOfMemoryException e)
                         {
                             Writer.Write(e.ToString(), "SEVERE");
+                            this.StopGame(0x02);
                         }
 
                         continue;
---
+++ SharpCraft/Core/SharpcraftWindow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
+namespace MyoKun.SharpCraft.Core
+{
+    public class SharpcraftWindow : GameWindow
+    {
+        private Sharpcraft Game;
+
+        public SharpcraftWindow(int width, int height, string title, Sharpcraft game)
+            : base(width, height, new GraphicsMode(32, 0, 0, 4), title)
+        {
+            this.Game = game;
+            Keyboard.KeyDown += handleKey;
+        }
+
+        private void handleKey(object sender, OpenTK.Input.KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Game.StopGame(0x00);
+            }
+        }
+    }
+}
