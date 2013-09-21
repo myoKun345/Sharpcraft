c4227911336640a833d6c4ea54f47a61991d4a50
Myo-kun <myo.kun345@gmail.com>
2013-09-20 23:23:34 -0400

    First stuff

---
+++ SharpCraft/Core/Sharpcraft.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+
+namespace MyoKun.SharpCraft.Core
+{
+    public class Sharpcraft
+    {
+        public string Username;
+        public LogWriter Writer = new LogWriterFactory().Create();
+        private Sharpcraft Instance;
+
+        public Boolean Running;
+
+        public Sharpcraft(string Username)
+        {
+            Instance = this;
+            this.Username = Username;
+            Writer.Write("Username: " + Username, "INFO");
+            Console.WriteLine(Username);
+        }
+
+        public void Run()
+        {
+            this.Running = true;
+
+            try
+            {
+                //this.StartGame();
+            }
+            catch (Exception e)
+            {
+                Writer.Write(e.ToString(), "SEVERE");
+            }
+
+            try
+            {
+                while (this.Running)
+                {
+                    if (this.Running)
+                    {
+                        try
+                        {
+                            //this.RunGameLoop;
+                        }
+                        catch (OutOfMemoryException e)
+                        {
+                            Writer.Write(e.ToString(), "SEVERE");
+                        }
+
+                        continue;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Writer.Write(e.ToString(), "SEVERE");
+            }
+        }
+    }
+}
---
+++ SharpCraft/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MyoKun.SharpCraft.Core;
+
+namespace MyoKun.SharpCraft
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int UserIndex = 1;
+            string Username = "Player" + rand.Next(1000);
+
+            for (int x = 0; x < args.Length; x++)
+            {
+                if (args[x] == "--username")
+                {
+                    UserIndex = x + 1;
+                    Username = args[UserIndex];
+                    break;
+                }
+            }
+
+            Sharpcraft sharpcraft = new Sharpcraft(Username);
+
+            sharpcraft.Run();
+        }
+    }
+}
